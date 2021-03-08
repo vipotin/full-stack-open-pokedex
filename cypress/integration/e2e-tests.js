5cf5291953d806dc1a92fc466b992a013f343e17
Vilma Pohjonen <vilma.pohjonen@eficode.com>
2021-03-08 16:00:38 +0200

    Add test for navigating to the next pokemon

--- cypress/integration/e2e-tests.js
+++ cypress/integration/e2e-tests.js
@@ -1,15 +1,23 @@
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
-    cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-  }),
+  // it('front page can be opened', function() {
+  //   cy.visit('http://localhost:5000')
+  //   cy.contains('ivysaur')
+  //   cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  // }),
 
-  it('pokemon page can be navigated to', function() {
+  // it('pokemon page can be navigated to', function() {
+  //   cy.visit('http://localhost:5000')
+  //   cy.get('div').contains('ivysaur').click()
+  //   cy.url().should('include', '/pokemon/ivysaur')
+  //   cy.get('.pokemon-name').contains('ivysaur')
+  //   cy.get('.pokemon-ability-name').contains('chlorophyll')
+  // })
+
+  it('next link displays the next pokemon', function() {
     cy.visit('http://localhost:5000')
     cy.get('div').contains('ivysaur').click()
     cy.url().should('include', '/pokemon/ivysaur')
-    cy.get('.pokemon-name').contains('ivysaur')
-    cy.get('.pokemon-ability-name').contains('chlorophyll')
+    cy.get('.links > a').contains('Next').click()
+    cy.get('.pokemon-name').contains('venusaur')
   })
 })
