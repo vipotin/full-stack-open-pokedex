267663a271e4662556eadc6715f459cb060a61cb
Vilma Pohjonen <vilma.pohjonen@eficode.com>
2021-03-08 16:28:05 +0200

    Broaden e2e testing

--- cypress/integration/e2e-tests.js
+++ cypress/integration/e2e-tests.js
@@ -1,17 +1,17 @@
 describe('Pokedex', function() {
-  // it('front page can be opened', function() {
-  //   cy.visit('http://localhost:5000')
-  //   cy.contains('ivysaur')
-  //   cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-  // }),
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
 
-  // it('pokemon page can be navigated to', function() {
-  //   cy.visit('http://localhost:5000')
-  //   cy.get('div').contains('ivysaur').click()
-  //   cy.url().should('include', '/pokemon/ivysaur')
-  //   cy.get('.pokemon-name').contains('ivysaur')
-  //   cy.get('.pokemon-ability-name').contains('chlorophyll')
-  // })
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('div').contains('ivysaur').click()
+    cy.url().should('include', '/pokemon/ivysaur')
+    cy.get('.pokemon-name').contains('ivysaur')
+    cy.get('.pokemon-ability-name').contains('chlorophyll')
+  })
 
   it('next link displays the next pokemon', function() {
     cy.visit('http://localhost:5000')
@@ -19,5 +19,13 @@ describe('Pokedex', function() {
     cy.url().should('include', '/pokemon/ivysaur')
     cy.get('.links > a').contains('Next').click()
     cy.get('.pokemon-name').contains('venusaur')
+  })
+
+  it('previous link displays the previous pokemon', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('div').contains('ivysaur').click()
+    cy.url().should('include', '/pokemon/ivysaur')
+    cy.get('.links > a').contains('Previous').click()
+    cy.get('.pokemon-name').contains('bulbasaur')
   })
 })
